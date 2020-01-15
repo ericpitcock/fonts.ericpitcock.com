402455fdcbab6201cfd76f5669642f6735806a55
Eric Pitcock <accounts+git@ericpitcock.com>
2020-01-14 23:15:58 -0800

    added calling code

--- src/App.vue
+++ src/App.vue
@@ -190,10 +190,15 @@ export default {
 
 <style lang="scss">
   /*! minireset.css v0.0.4 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}img,embed,iframe,object,video{height:auto;max-width:100%}audio{max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0;text-align:left}
+  @font-face {
+    font-family: 'CallingCode-Regular';
+    src: url('/fonts/390A5D_0_0.woff2') format('woff2');
+  }
   body {
-    margin: 0;
-    font-family: Helvetica, Arial, sans-serif;
+    font-family: 'CallingCode-Regular', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
     font-size: 14px;
+    // line-height: 18px;
+    color: black;
   }
   #app {
     height: 100%;
