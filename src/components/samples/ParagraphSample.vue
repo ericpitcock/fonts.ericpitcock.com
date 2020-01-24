0e7570a52c3e12f239db32409edc0de253fc9941
Eric Pitcock <accounts+git@ericpitcock.com>
2020-01-23 20:44:01 -0800

    dynamic line-height for paragraph sample

--- src/components/samples/ParagraphSample.vue
+++ src/components/samples/ParagraphSample.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="paragraph-sample">{{ getParagraph() }}</div>
+  <div class="paragraph-sample" :style="{ lineHeight: `${getGlobalFontSize * 1.3}px` }">
+    {{ getParagraph() }}
+  </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   var Chance = require('chance')
 
   export default {
     name: 'ParagraphSample',
+    computed: {
+      ...mapGetters([
+        'getGlobalFontSize'
+      ])
+    },
     methods: {
       getParagraph() {
         return chance.paragraph({ sentences: 3 })
