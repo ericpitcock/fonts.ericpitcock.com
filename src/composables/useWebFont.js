5fde1e9143402c52354bcf5cbad815b0507b0935
Eric Pitcock <accounts+git@ericpitcock.com>
2025-03-21 08:19:17 -0700

    don't load fonts already loaded

--- src/composables/useWebFont.js
+++ src/composables/useWebFont.js
@@ -1,21 +1,38 @@
 import { ref } from 'vue'
 import WebFont from 'webfontloader'
 
+// Module-level Set to track loaded fonts across all component instances
+const loadedFonts = new Set()
+
 export function useWebFont() {
   const loading = ref(true)
   const error = ref(false)
 
   const loadGoogleFonts = (fonts) => {
-    console.log('loading:', fonts)
+    // Filter out already loaded fonts
+    const fontsToLoad = fonts.filter(font => !loadedFonts.has(font))
+
+    // If all fonts are already loaded, just return
+    if (fontsToLoad.length === 0) {
+      console.log('All fonts already loaded:', fonts)
+      loading.value = false
+      return
+    }
+
+    console.log('Loading fonts:', fontsToLoad)
+    loading.value = true
+
     WebFont.load({
       google: {
-        families: fonts
+        families: fontsToLoad
       },
       classes: false,
       fontloading: () => {
         loading.value = true
       },
-      fontactive: () => {
+      fontactive: (familyName) => {
+        // Add the font to our cached set
+        loadedFonts.add(familyName)
         loading.value = false
       },
       fontinactive: () => {
