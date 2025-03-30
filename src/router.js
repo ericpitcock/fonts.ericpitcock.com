76afa9ed33dab894209aab9c04f0254d0515fffe
Eric Pitcock <accounts+git@ericpitcock.com>
2025-03-30 15:31:10 -0700

    no scrollBehavior

--- src/router.js
+++ src/router.js
@@ -46,13 +46,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { top: 0 }
-    }
-  }
 })
 
 router.beforeEach(async (to, from, next) => {
